25fdca70157241f58611a0910257a8c082abbd18
Marcin Majewski <marcin.majewski@sonarsource.com>
2020-08-05 14:44:12 +0200

    * Updaeˆ

--- src/main/java/testing/repo/App.java
+++ src/main/java/testing/repo/App.java
@@ -6,7 +6,7 @@ package testing.repo;
 public class App {
 
     public static final String STATIC_VARIABLE_TEST = "Hello `world1.";
-    public static final String NEXT_STATIC_VARIABLE = "TEST TEST";
+    public static final String NEXT_STATIC_VARIABLE = "TEsssST TEST";
 
     public String getGreeting() {
         return STATIC_VARIABLE_TEST;
---
+++ src/test/java/testing/repo/Blablablabla.java
@@ -0,0 +1,13 @@
+package testing.repo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Blablablabla {
+
+    @Test
+    public void testForPurposeOfTesting() {
+        assertEquals(2, 2);
+    }
+}
