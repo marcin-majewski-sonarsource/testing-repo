8e866ebacadaec0504b1ee9c8789551dd69fa51e
Marcin Majewski <marcin.majewski@sonarsource.com>
2020-08-05 09:10:54 +0200

    * Improving the code

--- src/main/java/testing/repo/App.java
+++ src/main/java/testing/repo/App.java
@@ -4,13 +4,17 @@
 package testing.repo;
 
 public class App {
+
+    public static final String STATIC_VARIABLE_TEST = "Hello `world1.";
+    public static final String NEXT_STATIC_VARIABLE = "TEST TEST";
+
     public String getGreeting() {
-        return "Hello world1.";
+        return STATIC_VARIABLE_TEST;
     }
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
         System.out.println(new App().getGreeting());
-        System.out.println("TEST TEST");
+        System.out.println(NEXT_STATIC_VARIABLE);
     }
 }
