a9c40f68e6f2f31031bac654c4276855d4e45da2
Marcin Majewski <marcin.majewski@sonarsource.com>
2020-08-05 09:14:44 +0200

    * Improving the code

--- src/test/java/testing/repo/AppTest.java
+++ src/test/java/testing/repo/AppTest.java
@@ -7,8 +7,16 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
+    @Test
+    public void testAppHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
+
+    @Test
+    public void testForSonarqube(){
+        App app = new App();
+        assertEquals("Hello `world1.", app.getGreeting());
+    }
+
 }
